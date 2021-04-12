9f1e9ad5a71bc9b653177f56b128d1eaafe3c121
Woody <77442862+WoodyGH@users.noreply.github.com>
2021-04-12 09:40:17 -0700

    plot3

---
+++ plot3.R
@@ -0,0 +1,21 @@
+library(dplyr)
+
+#read raw data
+FilePath = "/Users/Chungw/Documents/R/ExData_Plotting1/"
+FileName = paste0(FilePath, "household_power_consumption.txt")
+all_data <- read.table(FileName, header=TRUE, sep=";", na.string="?")
+
+#subset data based on the date
+sub_data <- subset(all_data, Date == "1/2/2007" | Date == "2/2/2007")
+
+#convert date and time characters to POSIXct
+sub_data <- sub_data %>% mutate(POSIX = as.POSIXct(strptime(paste(Date, Time, sep=" "), format="%d/%m/%Y %H:%M:%S"))) 
+
+#plot line graph of Energy sub metering
+with(sub_data, plot(POSIX, Sub_metering_1, col="black", type="l", xlab="", ylab="Energy sub metering", main=""))
+with(sub_data, lines(POSIX, Sub_metering_2, col="red"))
+with(sub_data, lines(POSIX, Sub_metering_3, col="blue"))
+legend("topright", legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"), lty=1, col=c("black","red","blue"))
+dev.copy(png, paste0(FilePath,"plot3.png"))
+dev.off()
+
